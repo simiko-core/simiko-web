8e61dafe909753796883463abb980d6038985db8
Achmad Rizki Ramadhan <achmadrizkiramadhan0101@gmail.com>
2025-06-26 12:11:44 +0700

    Update API middleware configuration to include 'web' middleware for improved access control to API documentation.

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -65,7 +65,7 @@ return [
              * Middleware allows to prevent unexpected access to API documentation
              */
             'middleware' => [
-                'api' => [],
+                'api' => ['web'],
                 'asset' => [],
                 'docs' => [],
                 'oauth2_callback' => [],
