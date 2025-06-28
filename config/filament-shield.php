ec86bab78306ba89ff8b2297b58b574bc536e607
Achmad Rizki Ramadhan <achmadrizkiramadhan0101@gmail.com>
2025-06-28 13:30:14 +0700

    Update filament-shield configuration to disable navigation registration and adjust widget list formatting for improved clarity.

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -2,7 +2,7 @@
 
 return [
     'shield_resource' => [
-        'should_register_navigation' => true,
+        'should_register_navigation' => false,
         'slug' => 'shield/roles',
         'navigation_sort' => -1,
         'navigation_badge' => true,
@@ -72,7 +72,8 @@ return [
         ],
 
         'widgets' => [
-            'AccountWidget', 'FilamentInfoWidget',
+            'AccountWidget',
+            'FilamentInfoWidget',
         ],
 
         'resources' => [],
